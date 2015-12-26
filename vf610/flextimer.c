f6a655cbb33718b1eb1d9bd923dc91fc7406e364
Werner2005 <webmaster@andy89.org>
2015-12-26 17:10:16 +0100

    update flextimer interface to new Interface

--- vf610/flextimer.c
+++ vf610/flextimer.c
@@ -1,5 +1,7 @@
 #include <vector.h>
-#include <flextimer.h>
+#include <timer.h>
+#define TIMER_PRV
+#include <timer_prv.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -9,6 +11,88 @@
 #include <core_cm4.h>
 #include <core_cmInstr.h>
 
+#define FTM_PRESCALE_MASK (0x7 << 0)
+#define FTM_PRESCALE(x) (((x) & 0x7) << 0)
+#define FTM_SELECT_CLK_MASK (0x3 << 3)
+#define FTM_SELECT_CLK(x) (((x) & 0x3) << 3)
+#define FTM_INTERRUPT_EN BIT(6)
+#define FTM_OVERFLOWED BIT(7)
+#define FTM_IS_OVERFLOWED(x) (((x) >> 7) & 0x1)
+
+#define FTM_CLK_DISABLE 0x0
+#define FTM_CLK_SYSTEM 0x1
+#define FTM_CLK_FIXED 0x2
+#define FTM_CLK_EXTERN 0x3
+
+#define FTM_MODE_EN BIT(0)
+#define FTM_MODE_WR_PROTECT_DIS BIT(2)
+#define FTM_MODE_WR_PROTECT_IS_DIS(x) (((x) >> 2) & 0x1)
+#define FTM_FMS_WR_PROTECT_EN BIT(6)
+#define FTM_FMS_WR_PROTECT_IS_EN(x) (((x) >> 6) & 0x1)
+
+#define FTM_CNSC_DMA (1 << 0) 
+#define FTM_CNSC_ELSA (1 << 2)
+#define FTM_CNSC_ELSB (1 << 3)
+#define FTM_CNSC_MSA (1 << 4)
+#define FTM_CNSC_MSB (1 << 5)
+#define FTM_CNSC_CHIE (1 << 6)
+#define FTM_CNSC_CHF (1 << 7)
+#define FTM_CNSC_IS_CHF(x) ((x >> 7) & 0x1)
+
+struct ftm_channel{
+    uint32_t csc;
+    uint32_t cv;
+};
+
+struct ftm_reg {
+	uint32_t sc;
+	uint32_t cnt;
+	uint32_t mod;
+	struct ftm_channel ch[8];
+	uint32_t cntin;
+	uint32_t status;
+	uint32_t mode;
+	uint32_t sync;
+	uint32_t outinit;
+	uint32_t outmask;
+	uint32_t combine;
+	uint32_t deadtime;
+	uint32_t exttrig;
+	uint32_t pol;
+	uint32_t fms;
+	uint32_t filter;
+	uint32_t fltctrl;
+	uint32_t qdctrl;
+	uint32_t conf;
+	uint32_t fltpol;
+	uint32_t syncconf;
+	uint32_t invctrl;
+	uint32_t swoctrl;
+	uint32_t pwmload;
+};
+
+typedef enum {
+	NOT_CONFIG,
+	PERIODIC,
+	ONESHOT
+} ftm_mode_t;
+
+struct timer {
+	struct timer_generic gen;
+	struct ftm_reg *base;
+	ftm_mode_t mode;
+	uint32_t prescaler;
+	int32_t irqnr;
+	bool (*irqhandle)(struct timer *ftm, void *data);
+	void (*captureHandle)(struct timer *ftm, void *data, uint32_t channel);
+	bool isConfig;
+	void *overflowData;
+	void *captureData;
+	int32_t ftmid;
+	uint64_t basetime;
+	int64_t adjust;
+};
+
 #define IPG_FREQ 66ULL /* 66Mhz */
 
 #define VF610_PWM_GENERAL_CTRL (PAD_CTL_SPEED_HIGH | PAD_CTL_DSE_20ohm | PAD_CTL_IBE_ENABLE | PAD_CTL_PUS_100K_UP)
@@ -17,28 +101,6 @@
 #define VF610_FLEXTIMER_2 ((struct ftm_reg *) 0x400B8000)
 #define VF610_FLEXTIMER_3 ((struct ftm_reg *) 0x400B9000)
 
-struct ftm ftms[] = {
-	{
-		.base = VF610_FLEXTIMER_0,
-		.isConfig = false,
-		.irqnr = 42,
-	},
-	{
-		.base = VF610_FLEXTIMER_1,
-		.isConfig = false,
-		.irqnr = 43,
-	},
-	{
-		.base = VF610_FLEXTIMER_2,
-		.isConfig = false,
-		.irqnr = 44,
-	},
-	{
-		.base = VF610_FLEXTIMER_3,
-		.isConfig = false,
-		.irqnr = 45,
-	},
-};
 
 struct pwm_pin {
 	uint32_t pin;
@@ -148,7 +210,7 @@ struct pwm_pin pins[4][8] = {
 	}
 };
 
-static void clearIRQBit(struct ftm *ftm) {
+static void clearIRQBit(struct timer *ftm) {
 	volatile uint32_t tmp;
 	/* Disable write Protection not needed hear */
 	/* Workarount for Errata e6484 */
@@ -162,12 +224,12 @@ static void clearIRQBit(struct ftm *ftm) {
 	}
 }
 
-static void inline handleIRQ(struct ftm *ftm) {
+static void inline handleIRQ(struct timer *ftm) {
 	int i;
 	switch (ftm->mode) {
 		case ONESHOT:
 			ftm->mode = NOT_CONFIG;
-			ftm_stop(ftm);
+			timer_stop(ftm);
 			break;
 		default:
 			break;
@@ -189,42 +251,26 @@ static void inline handleIRQ(struct ftm *ftm) {
 	}
 	if (FTM_IS_OVERFLOWED(ftm->base->sc)) {
 		if (ftm->irqhandle) {
-			ftm->irqhandle(ftm, ftm->overflowData);
+			ftm->irqhandle(ftm, ftm->overflowData); /* TODO Handle bool;) */
 		}
 		clearIRQBit(ftm);
 	}
 }
 
-void flextimer0_isr() {
-	struct ftm *ftm = &ftms[0];
-	handleIRQ(ftm);
-}
-void flextimer1_isr() {
-	struct ftm *ftm = &ftms[1];
-	handleIRQ(ftm);
-}
-void flextimer2_isr() {
-	struct ftm *ftm = &ftms[2];
-	handleIRQ(ftm);
-}
-void flextimer3_isr() {
-	struct ftm *ftm = &ftms[3];
-	handleIRQ(ftm);
-}
 
-static inline void ftm_writeProtecEnable(struct ftm *ftm) {
+static inline void ftm_writeProtecEnable(struct timer *ftm) {
 	do {
 		ftm->base->fms |= FTM_FMS_WR_PROTECT_EN;
 	} while(FTM_MODE_WR_PROTECT_IS_DIS(ftm->base->mode));
 
 }
-static inline void ftm_writeProtecDisable(struct ftm *ftm) {
+static inline void ftm_writeProtecDisable(struct timer *ftm) {
 	/* make sure pritec Mode is disabled */
 	do {
 		ftm->base->mode |= FTM_MODE_WR_PROTECT_DIS;
 	} while(FTM_FMS_WR_PROTECT_IS_EN(ftm->base->fms));
 }
-int32_t ftm_start(struct ftm *ftm) {
+TIMER_START(ftm, ftm) {
 	/* Start Only if mode is setup */
 	if (ftm->mode != NOT_CONFIG) {
 		ftm_writeProtecDisable(ftm);
@@ -235,7 +281,7 @@ int32_t ftm_start(struct ftm *ftm) {
 	return 0;
 }
 
-int32_t ftm_stop(struct ftm *ftm) {
+TIMER_STOP(ftm, ftm) {
 	ftm_writeProtecDisable(ftm);
 	/* Clear Clock Selection and disable clock */
 	ftm->base->sc &= ~FTM_SELECT_CLK_MASK;
@@ -249,7 +295,7 @@ int32_t ftm_stop(struct ftm *ftm) {
 	clearIRQBit(ftm);
 	return 0;
 }
-static int32_t configureFtm(struct ftm *ftm, uint64_t us) {
+static int32_t configureFtm(struct timer *ftm, uint64_t us) {
 	uint64_t counterValue;
 	if (us != UINT64_MAX) {
 		us = (us * (ftm->basetime + ftm->adjust)) / ftm->basetime;
@@ -266,29 +312,29 @@ static int32_t configureFtm(struct ftm *ftm, uint64_t us) {
 	/* 
 	 * Stop Timer befor configure
 	 */
-	ftm_stop(ftm);
+	timer_stop(ftm);
 
 	ftm_writeProtecDisable(ftm);
 	ftm->base->cnt = 0;
 	ftm->base->mod = counterValue;
 	ftm_writeProtecEnable(ftm);
 
-	ftm_start(ftm);
+	timer_start(ftm);
 	return 0;
 }
 
-int32_t ftm_oneshot(struct ftm *ftm, uint64_t us) {
+TIMER_ONESHOT(ftm, ftm, us) {
 	ftm->mode = ONESHOT;
 	return configureFtm(ftm, us);
 }
 
 
-int32_t ftm_periodic(struct ftm *ftm, uint64_t us) {
+TIMER_PERIODIC(ftm, ftm, us) {
 	ftm->mode = PERIODIC;
 	return configureFtm(ftm, us);
 }
 
-uint64_t ftm_getTime(struct ftm *ftm) {
+TIMER_GET_TIME(ftm, ftm) {
 	uint64_t value;
 
 	/* read the epit */
@@ -300,7 +346,7 @@ uint64_t ftm_getTime(struct ftm *ftm) {
 	return us;
 }
 
-static int32_t setupChannelPin(struct ftm *ftm, uint32_t channel) {
+static int32_t setupChannelPin(struct timer *ftm, uint32_t channel) {
 	int32_t ret;
 	struct mux *mux = mux_init();
 	struct pwm_pin *pin = &pins[ftm->ftmid][channel];
@@ -314,7 +360,7 @@ static int32_t setupChannelPin(struct ftm *ftm, uint32_t channel) {
 	return 0;
 }
 
-int32_t ftm_setupPWM(struct ftm *ftm, uint32_t channel) {
+int32_t ftm_setupPWM(struct timer *ftm, uint32_t channel) {
 	int32_t ret;
 	ret = setupChannelPin(ftm, channel);
 	if (ret < 0) {
@@ -327,7 +373,7 @@ int32_t ftm_setupPWM(struct ftm *ftm, uint32_t channel) {
 
 	return 0;	
 }
-int32_t ftm_setPWMDutyCycle(struct ftm *ftm, uint32_t channel, uint64_t us) {
+int32_t ftm_setPWMDutyCycle(struct timer *ftm, uint32_t channel, uint64_t us) {
 	uint64_t counterValue;
 	us = (us * (ftm->basetime + ftm->adjust)) / ftm->basetime;
 	counterValue = (uint64_t) (IPG_FREQ * us) / ((ftm->prescaler + 1));
@@ -345,7 +391,7 @@ int32_t ftm_setPWMDutyCycle(struct ftm *ftm, uint32_t channel, uint64_t us) {
 	return 0;
 }
 
-int32_t ftm_setupCapture(struct ftm *ftm, uint32_t channel) {
+int32_t ftm_setupCapture(struct timer *ftm, uint32_t channel) {
 	int32_t ret;
 	ret = setupChannelPin(ftm, channel);
 	if (ret < 0) {
@@ -359,7 +405,7 @@ int32_t ftm_setupCapture(struct ftm *ftm, uint32_t channel) {
 
 	return 0;
 }
-int64_t ftm_getChannelTime(struct ftm *ftm, uint32_t channel) {
+int64_t ftm_getChannelTime(struct timer *ftm, uint32_t channel) {
 	uint64_t value = ftm->base->ch[channel].cv;
 	uint64_t us = (value / ((uint64_t)IPG_FREQ / ftm->prescaler));
 	/* Time Adjust */ 
@@ -367,14 +413,14 @@ int64_t ftm_getChannelTime(struct ftm *ftm, uint32_t channel) {
 	return us;
 }
 
-struct ftm *ftm_init(uint32_t index, uint32_t prescaler, uint64_t basetime, int64_t adjust) {
+TIMER_INIT(ftm, index, prescaler, basetime, adjust) {
 	int i; 
-	struct ftm *ftm;;
+	struct timer *ftm;;
 	if (index > 3) {
 		return NULL;
 	}
-	ftm = &ftms[index];
-	if (ftm->isConfig) {
+	ftm = timers[index];
+	if (hal_isInit(ftm)) {
 		return ftm;
 	}
 	ftm->ftmid = index;
@@ -434,18 +480,66 @@ struct ftm *ftm_init(uint32_t index, uint32_t prescaler, uint64_t basetime, int6
 	irq_enable(ftm->irqnr);
 	return ftm;
 }
-int32_t ftm_setOverflowHandler(struct ftm *ftm, void (*irqhandle)(struct ftm *ftm, void *data), void *data) {
+TIMER_SET_OVERFLOW_CALLBACK(ftm, ftm, irqhandle, data) {
 	ftm->irqhandle = irqhandle;
 	ftm->overflowData = data;
 	ftm->base->sc |= FTM_INTERRUPT_EN;
 	return 0;
 }
-int32_t ftm_setCaptureHandler(struct ftm *ftm, void (*irqhandle)(struct ftm *ftm, void *data, uint32_t channel), void *data) {
+int32_t ftm_setCaptureHandler(struct timer *ftm, void (*irqhandle)(struct timer *ftm, void *data, uint32_t channel), void *data) {
 	ftm->captureHandle = irqhandle;
 	ftm->captureData = data;
 	return 0;
 }
-int32_t ftm_deinit(struct ftm *ftm) {
-	ftm_stop(ftm);
+TIMER_DEINIT(ftm, ftm) {
+	timer_stop(ftm);
 	return 0;
+}
+
+TIMER_OPS(ftm);
+
+struct timer ftm_timer_0 =  {
+	TIMER_INIT_DEV(ftm)
+	.base = VF610_FLEXTIMER_0,
+	.isConfig = false,
+	.irqnr = 42,
+};
+TIMER_ADDDEV(ftm, ftm_timer_0);
+struct timer ftm_timer_1 = {
+	TIMER_INIT_DEV(ftm)
+	.base = VF610_FLEXTIMER_1,
+	.isConfig = false,
+	.irqnr = 43,
+};
+TIMER_ADDDEV(ftm, ftm_timer_1);
+struct timer ftm_timer_2 = {
+	TIMER_INIT_DEV(ftm)
+	.base = VF610_FLEXTIMER_2,
+	.isConfig = false,
+	.irqnr = 44,
+};
+TIMER_ADDDEV(ftm, ftm_timer_2);
+struct timer ftm_timer_3 = {
+	TIMER_INIT_DEV(ftm)
+	.base = VF610_FLEXTIMER_3,
+	.isConfig = false,
+	.irqnr = 45,
+};
+TIMER_ADDDEV(ftm, ftm_timer_3);
+
+void flextimer0_isr() {
+	struct timer *ftm = &ftm_timer_0;
+	handleIRQ(ftm);
+}
+void flextimer1_isr() {
+	struct timer *ftm = &ftm_timer_1;
+	handleIRQ(ftm);
+}
+void flextimer2_isr() {
+	struct timer *ftm = &ftm_timer_2;
+	handleIRQ(ftm);
+}
+void flextimer3_isr() {
+	struct timer *ftm = &ftm_timer_3;
+	handleIRQ(ftm);
 }
