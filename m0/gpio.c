f481b095df7a747cce785b877ffc56031381d310
Werner2005 <webmaster@andy89.org>
2016-03-12 12:38:04 +0100

    update m0 sim to new interface

--- m0/gpio.c
+++ m0/gpio.c
@@ -1,31 +1,26 @@
 #include <FreeRTOS.h>
 #include <stdint.h>
 #include <gpio.h>
+#define GPIO_PRV
+#include <gpio_prv.h>
 #include "iomux.h"
 
 
 struct gpio {
+	struct gpio_generic gen;
 	uint32_t dummy;
-};
-struct gpio gpio = {
-	.dummy = 0,
 };
 struct gpio_pin {
 	uint32_t dummy;
 };
 
-struct gpio *gpio_init() {
-	return &gpio;
+GPIO_INIT(m0, index) {
+	return gpios[index];
 }
-int32_t gpio_deinit(struct gpio *gpio) {
+GPIO_DEINIT(m0, g) {
 	return 0;
 }
-#define HMI2015_GPIO_GENERAL_CTRL (PAD_CTL_PKE | PAD_CTL_PUE | PAD_CTL_SPEED_MED | PAD_CTL_PUS_47K_UP | \
-		PAD_CTL_DSE_25ohm)
-int32_t gpio_setDirection(struct gpio_pin *pin, enum gpio_direction dir) {
-	return 0;
-}
-struct gpio_pin *gpioPin_init(struct gpio *gpio, uint8_t pin, enum gpio_direction dir, enum gpio_setting setting) {
+GPIO_PIN_INIT(m0, g, pin, dir, setting) {
 	struct gpio_pin *gpio_pin= pvPortMalloc(sizeof(struct gpio_pin));
 	if (gpio_pin == NULL) {
 		goto gpio_getPin_error0;
@@ -37,40 +32,47 @@ struct gpio_pin *gpioPin_init(struct gpio *gpio, uint8_t pin, enum gpio_directio
 gpio_getPin_error0:
 	return NULL;
 }
-int32_t gpioPin_deinit(struct gpio_pin *pin) {
+GPIO_PIN_DEINIT(m0, pin) {
 	vPortFree(pin);
+	return 0; /* TODO */
+}
+GPIO_PIN_SET_VALUE(m0, pin, value) {
 	return 0;
 }
-int32_t gpioPin_setValue(struct gpio_pin *pin, bool value) {
+GPIO_PIN_SET_PIN(m0, pin) {
 	return 0;
 }
-int32_t gpioPin_setPin(struct gpio_pin *pin) {
+GPIO_PIN_CLEAR_PIN(m0, pin) {
 	return 0;
 }
-int32_t gpioPin_clearPin(struct gpio_pin *pin) {
+GPIO_PIN_TOGGLE_PIN(m0, pin) {
 	return 0;
 }
-int32_t gpioPin_togglePin(struct gpio_pin *pin) {
-	return 0;
-}
-bool gpioPin_getValue(struct gpio_pin *pin) {
+GPIO_PIN_GET_VALUE(m0, pin) {
 	return true;
 }
-int32_t gpioPin_enableInterrupt(struct gpio_pin *pin) {
+GPIO_PIN_ENABLE_INTERRUPT(m0, pin) {
+	return -1;
+}
+GPIO_PIN_DISABLE_INTERRUPT(m0, pin) {
+	return -1;
+}
+GPIO_PIN_SET_CALLBACK(m0, pin, callback, data, inter) {
+	return -1;
+}
+GPIO_PIN_SET_DIRECTION(m0, pin, dir) {
 	return 0;
 }
-int32_t gpioPin_disableInterrupt(struct gpio_pin *pin) {
+GPIO_PIN_SET_SETTING(m0, pin, setting) {
 	return 0;
 }
-int32_t gpioPin_setCallback(struct gpio_pin *pin, bool (*calback)(struct gpio_pin *pin, void *data), void *data, enum gpio_interrupt inter) {
+GPIO_PIN_SCHMITT_TRIGGER(m0, pin, schmitt) {
 	return 0;
 }
-int32_t gpioPin_setDirection(struct gpio_pin *pin, enum gpio_direction dir) {
-	return 0;
-}
-int32_t gpioPin_setSetting(struct gpio_pin *pin, enum gpio_setting setting) {
-	return 0;
-}
-int32_t gpioPin_SchmittTrigger(struct gpio_pin *pin, bool schmit) {
-	return 0;
-}
+GPIO_OPS(m0);
+
+struct gpio gpio = {
+	GPIO_INIT_DEV(m0)
+	.dummy = 0,
+};
+GPIO_ADDDEV(m0, gpio);
