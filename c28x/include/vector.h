9535adb956081958362c0c8b4c0dc7134dcf41c4
Andreas Werner <webmaster@andy89.org>
2020-12-05 12:19:41 +0100

    add missing definition for CPU Interrupt

--- c28x/include/vector.h
+++ c28x/include/vector.h
@@ -150,7 +150,25 @@ typedef enum {
 	rsvd12_6_IRQn, //!< Reserved
 	CLAINT_LVF_IRQn, //!< CLA Interrupt LVF
 	CLAINT_LUF_IRQn, //!< CLA Interrupt LUF
-IRQ_COUNT
+	IRQ_COUNT
 } IRQn_t;
+extern __cregister volatile unsigned int IER;
+extern __cregister volatile unsigned int IFR;
+#define M_INT1  0x0001
+#define M_INT2  0x0002
+#define M_INT3  0x0004
+#define M_INT4  0x0008
+#define M_INT5  0x0010
+#define M_INT6  0x0020
+#define M_INT7  0x0040
+#define M_INT8  0x0080
+#define M_INT9  0x0100
+#define M_INT10 0x0200
+#define M_INT11 0x0400
+#define M_INT12 0x0800
+#define M_INT13 0x1000
+#define M_INT14 0x2000
+#define M_DLOG  0x4000
+#define M_RTOS  0x8000
 int32_t irq_setHandler(int32_t irqnr, void (*irq_handler)());
 #endif
