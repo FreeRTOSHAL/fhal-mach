23ff6b83203dbd701e935de3e9c7c8d4c2fd09e6
Jonathan Klamroth <jonathan.klamroth@gmail.com>
2021-01-31 21:55:52 +0100

    add ECAN devices to devs.h

--- c28x/include/devs.h
+++ c28x/include/devs.h
@@ -11,6 +11,10 @@ HAL_DEFINE_GLOBAL_ARRAY(uart);
 # define SCI1_ID HAL_GET_ID(uart, sci, sci1)
 #endif
 HAL_DEFINE_GLOBAL_ARRAY(timer);
+#ifdef CONFIG_MACH_C28X_ECAN0
+# define ECAN0_ID HAL_GET_ID(can, ecan, ecan0)
+#endif
+HAL_DEFINE_GLOBAL_ARRAY(can);
 #ifdef CONFIG_MACH_C28X_CPU_TIMER0
 # define CPU_TIMER0_ID HAL_GET_ID(timer, c28x, cpu_timer0)
 #endif
